f0a91a1a82a4b007f70dbe351d3aa5d044732bea
RookPeace <rookpeace@gmail.com>
2023-05-24 20:03:16 -0400

    created back end server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+
+
+
+
+
+
+
+
+app.listen(3000, console.log("server has started on port 3000"));
