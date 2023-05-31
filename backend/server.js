1ad03cfd3b93546254f64e7bc762f26f8051b1ed
RookPeace <rookpeace@gmail.com>
2023-05-31 16:24:47 -0400

    added data.js to test server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-const app = express();
+const dotenv = require("dotenv")
 
 
+const { chats } = require("./data/data");
 
+const app = express();  
+dotenv.config();                            
 
+app.get("/",(req, res) => {
+    res.send("Api is running");
+});
 
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
 
+app.get("/api/chat/id", (req, res) => {
+   // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
 
-
-
-app.listen(3000, console.log("server has started on port 3000"));
+const PORT = process.env.PORT;
+app.listen(3000, console.log('Server started on PORT ${PORT}' ));
