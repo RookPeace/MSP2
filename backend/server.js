7e264b1409da782a175dd79118c24480dba7e668
RookPeace <rookpeace@gmail.com>
2023-05-31 18:03:29 -0400

    fixed export for test data

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.get("/api/chat", (req, res) => {
     res.send(chats);
 });
 
-app.get("/api/chat/id", (req, res) => {
+app.get("/api/chat/:id", (req, res) => {
    // console.log(req.params.id);
     const singleChat = chats.find((c) => c._id === req.params.id);
     res.send(singleChat);
