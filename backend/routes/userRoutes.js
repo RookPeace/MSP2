803f53f4da9d65bb9a1ed795c665688a8f0f083f
RookPeace <rookpeace@gmail.com>
2023-06-26 15:31:50 -0400

    completed chat features

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const { registerUser,authUser, allUsers } = require("../controllers/userControllers");
 const router = express.Router();
 router.use(express.json());
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/').post(registerUser).get(allUsers);
+
+router.route('/').post(registerUser).get(protect,allUsers);
 router.post('/login', authUser)
 
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const connectDB = require("./config/db");
 const { chats } = require("./data/data");
 const colors = require("colors");
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();  
 connectDB()
 const app = express();  
@@ -14,7 +16,8 @@ app.get("/",(req, res) => {
     res.send("Api is running");
 });
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 
 
 
